16082c0b0cedb8d4e44b4a099a9ad843414b66f3
koszyk91 <michal@joynet.org.pl>
2023-04-16 12:06:43 +0200

    Zadanie dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// zadanie dzień 4
+string name;
+char sex;
+byte age;
+
+name = "Ewa";
+sex = 'F';
+age = 33;
+
+if (sex != 'F')
+{
+    if (age < 18)
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    else
+    {
+        Console.WriteLine("Mężczyzna powyżej 18 lat");
+    }
+}
+else
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+        
+    if (age >= 30)
+        {
+        if (name == "Ewa" && age == 33)
+            Console.WriteLine("Ewa, lat 33");
+        else Console.WriteLine("To już starsza Pani jest");
+        }
+}
